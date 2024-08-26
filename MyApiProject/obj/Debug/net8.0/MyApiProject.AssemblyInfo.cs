20a77950a8c8b4a9ebd0255c8018acf3fd140db9
unknown <penelopemaake@gmail.com>
2024-08-26 13:41:53 +0200

    post api to register students

--- MyApiProject/obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
+++ MyApiProject/obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc20e3ce442112bc83746a111487db5e46977204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ded45db241e326c68b1192532e1db6ba4212bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
